e254ddb866358e910f9b967ec06d49883bcb056e
Pierre GINDRAUD <pgindraud+github@gmail.com>
2016-08-01 00:26:25 +0200

    Add some tests scripts

---
+++ tests/parser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+env1 = dict(
+SMS_MESSAGES="1",
+DECODED_PARTS="0",
+SMS_1_NUMBER="0124",
+SMS_1_CLASS="-1",
+SMS_1_TEXT="ghgg",
+)
+
+env2 = dict(
+SMS_2_CLASS="-1",
+SMS_MESSAGES="3",
+DECODED_PARTS="1",
+SMS_3_NUMBER="0124",
+SMS_3_TEXT="ernvgigfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfjcuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffyernvgi",
+SMS_2_TEXT="cuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffyernvgigfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfjcuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffy",
+SMS_2_NUMBER="0124",
+SMS_3_CLASS="-1",
+DECODED_0_TEXT="ijhgfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfjcuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffyernvgifoxhidtuhgfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfjcuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffyernvgigfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfjcuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffyernvgigfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfjcuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffyernvgi",
+SMS_1_NUMBER="0124",
+SMS_1_CLASS="-1",
+SMS_1_TEXT="ijhgfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfjcuhdy8hdyujftucdyjxyhfyhrijfiigujugkcjtfdskfgjhfyhffyernvgifoxhidtuhgfhhykfkkfdknxdhkjxhkkckjghfkbfujvjjxtyfj"
+)
+
+env = env2
+
+for key in env:
+  os.environ[key] = env[key]
+
+subprocess.check_output(['../utils/sms-shell-parser.py', '--output=../fifo'])
