2a83c17edf6708312532a552d657e23a6a108980
Pierre GINDRAUD <pgindraud@gmail.com>
2016-09-02 21:40:48 +0200

    Add documentation

--- SMSShell/exceptions.py
+++ SMSShell/exceptions.py
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"""
+"""This module define all operations related Exceptions
 """
 
 class SMSException(Exception):
