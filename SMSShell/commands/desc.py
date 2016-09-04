634755d409b643d4c29715c7205418aa0693719c
Pierre GINDRAUD <pgindraud@gmail.com>
2016-09-04 13:37:06 +0200

    Add desc command

---
+++ SMSShell/commands/desc.py
@@ -0,0 +1,51 @@
+# -*- coding: utf8 -*-
+
+# This file is a part of SMSShell
+#
+# Copyright (c) 2016 Pierre GINDRAUD
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Desc command
+
+This command return a short description of what a command do
+"""
+
+from . import AbstractCommand
+from ..exceptions import CommandException
+
+class Command(AbstractCommand):
+
+  def argsProperties(self):
+    return dict(min=1, max=1)
+
+  def inputStates(self):
+    return []
+
+  def usage(self, argv):
+    return 'desc COMMAND'
+
+  def description(self, argv):
+    return 'Show commands short description'
+
+  def main(self, argv):
+    try:
+      return self.shell.getCommand(self.session, argv[0]).description(argv[1:])
+    except CommandException:
+      return 'command not available'
