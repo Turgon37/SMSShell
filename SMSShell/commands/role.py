9a27bebd32cedfab2560bdc851075213672576e9
Pierre GINDRAUD <pgindraud@gmail.com>
2017-08-13 09:54:33 +0200

    Add whoami and role commands

---
+++ SMSShell/commands/role.py
@@ -0,0 +1,43 @@
+# -*- coding: utf8 -*-
+
+# This file is a part of SMSShell
+#
+# Copyright (c) 2016-2017 Pierre GINDRAUD
+#
+# SMSShell is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SMSShell is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SMSShell. If not, see <http://www.gnu.org/licenses/>.
+
+"""Role command
+
+This command return the current role name
+"""
+
+from . import AbstractCommand
+
+
+class Command(AbstractCommand):
+
+    def argsProperties(self):
+        return dict()
+
+    def inputStates(self):
+        return []
+
+    def usage(self, argv):
+        return 'role'
+
+    def description(self, argv):
+        return 'print current role'
+
+    def main(self, argv):
+        return str(self.session.state.name)
