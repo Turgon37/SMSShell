e0667f1bde60c11dae45f319416a298bbcbe7f58
Pierre GINDRAUD <pgindraud@gmail.com>
2018-01-14 11:34:56 +0100

    update copyrights

--- utils/sms-shell-parser.py
+++ utils/sms-shell-parser.py
@@ -2,7 +2,7 @@
 
 # This file is a part of SMSShell
 #
-# Copyright (c) 2016-2017 Pierre GINDRAUD
+# Copyright (c) 2016-2018 Pierre GINDRAUD
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
