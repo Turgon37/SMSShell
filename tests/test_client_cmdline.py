eec754c4da3ec71110eb6366f970b4a7544922f3
Pierre GINDRAUD <pgindraud+github@gmail.com>
2018-05-11 20:21:39 +0200

    fix test

--- tests/test_client_cmdline.py
+++ tests/test_client_cmdline.py
@@ -7,16 +7,16 @@ import subprocess
 # command line test
 def test_cmdline_help():
     """Must produce an error is no url was given"""
-    result = subprocess.Popen(shlex.split('./utils/sms-shell-client --help'), stdout=subprocess.PIPE)
+    result = subprocess.Popen(shlex.split('./bin/sms-shell-client --help'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
     assert 'usage:' in stdout.decode()
 
 def test_cmdline_version():
-    result = subprocess.Popen(shlex.split('./utils/sms-shell-client --version'), stdout=subprocess.PIPE)
+    result = subprocess.Popen(shlex.split('./bin/sms-shell-client --version'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
     assert 'SMSShell client version' in stdout.decode()
 
 def test_cmdline_without_config_file():
-    result = subprocess.Popen(shlex.split('./utils/sms-shell-client'), stdout=subprocess.PIPE)
+    result = subprocess.Popen(shlex.split('./bin/sms-shell-client'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
     assert result.returncode == 0
