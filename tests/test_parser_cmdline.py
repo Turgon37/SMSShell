a4841e0d01d2ab78d81b964b1f88012e9ddb799d
Pierre GINDRAUD <pgindraud+github@gmail.com>
2018-05-11 20:21:39 +0200

    fix test

--- tests/test_parser_cmdline.py
+++ tests/test_parser_cmdline.py
@@ -7,16 +7,16 @@ import subprocess
 # command line test
 def test_cmdline_help():
     """Must produce an error is no url was given"""
-    result = subprocess.Popen(shlex.split('./utils/sms-shell-parser --help'), stdout=subprocess.PIPE)
+    result = subprocess.Popen(shlex.split('./bin/sms-shell-parser --help'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
     assert 'usage:' in stdout.decode()
 
 def test_cmdline_version():
-    result = subprocess.Popen(shlex.split('./utils/sms-shell-parser --version'), stdout=subprocess.PIPE)
+    result = subprocess.Popen(shlex.split('./bin/sms-shell-parser --version'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
     assert 'SMSShell parser version' in stdout.decode()
 
 def test_cmdline_without_config_file():
-    result = subprocess.Popen(shlex.split('./utils/sms-shell-parser'), stdout=subprocess.PIPE)
+    result = subprocess.Popen(shlex.split('./bin/sms-shell-parser'), stdout=subprocess.PIPE)
     stdout, stderr = result.communicate()
     assert result.returncode == 0
