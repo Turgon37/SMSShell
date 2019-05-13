e7d00c8948ac8ce15b7f6a1e98f189828337f2a5
Pierre GINDRAUD <pgindraud@gmail.com>
2019-05-14 00:23:22 +0200

    Fix transmitter tests

--- tests/test_smsshell_transmitter_python_gammu.py
+++ tests/test_smsshell_transmitter_python_gammu.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+import configparser
 import pytest
 import os
 
@@ -27,12 +28,15 @@ def test_without_read_right():
 def test_simple_transmit():
     """Test base parser class exception
     """
+    smsdrc = configparser.ConfigParser()
+    smsdrc['gammu'] = dict(Device='/dev/null')
+    smsdrc['smsd'] = dict(Service='FILES')
     with open('tests/smsdrc', 'w') as w:
-        pass
-    config = dict(umask='9', smsdrc_configuration='tests/smsdrc')
+        smsdrc.write(w)
+    config = dict(umask='9', smsdrc_configuration=os.path.join(os.getcwd(), 'tests/smsdrc'))
     transmitter = SMSShell.transmitters.python_gammu.Transmitter(config=config)
     transmitter.start()
     message = SMSShell.models.Message('local', 'OK')
     transmitter.transmit(message)
     transmitter.stop()
-    os.unlink('tests/smsdrc')
+    #os.unlink('tests/smsdrc')
